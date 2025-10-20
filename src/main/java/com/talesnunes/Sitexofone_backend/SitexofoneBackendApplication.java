6c1ebd3ff1fa3d5d2760c9897e84e326adaf3a88
Tales-Nunes <talesnunes.siqueira@gmail.com>
2025-10-20 13:22:22 -0300

    feat: Criação do projeto.

---
+++ src/main/java/com/talesnunes/Sitexofone_backend/SitexofoneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.talesnunes.Sitexofone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitexofoneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitexofoneBackendApplication.class, args);
+	}
+
+}
